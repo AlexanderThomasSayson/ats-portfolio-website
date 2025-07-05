702bd3cc9c01070717c9ab8523cd1f5c51e82f9c
Alexander <alexander@solutionsresource.com>
2025-07-05 14:33:31 +0800

    fix: clean folder structure.

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { Navbar } from "./navbar/Navbar";
 export { MobileMenu } from "./navbar/MobileMenu";
-export { Home } from "./navbar/Home";
-export { About } from "./navbar/About";
-export {Logo} from "./navbar/Logo"
+export { Home } from "./sections/home/Home.jsx";
+export { About } from "./sections/about/About.jsx";
+export {Logo} from "./sections/logos/Logo.jsx"
 export { LoadingScreen } from "./loading-screen/LoadingScreen";
